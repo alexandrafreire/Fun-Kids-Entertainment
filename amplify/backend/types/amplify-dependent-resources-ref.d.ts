200b4760eda16efd7047e258ea7e4904c24111eb
alexandrabfreire <alexandrabfreire@gmail.com>
2023-04-19 23:01:33 +0100

    remove function 2

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -7,15 +7,6 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "auth": {
-    "FunKidsEntertainment": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    },
     "userPoolGroups": {
       "adminGroupRole": "string"
     }
