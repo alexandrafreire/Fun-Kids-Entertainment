3560d6cf5d7e63e21d96b4a411c05a9f2ddd8ec0
alexandrabfreire <alexandrabfreire@gmail.com>
2023-08-01 00:47:49 +0100

    remove resources folder

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d 3.ts
@@ -0,0 +1,58 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "AdminQueries": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
+    "funkidsentertainmentAPI": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "FunKidsEntertainment": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "AppUsersGroupRole": "string",
+      "adminGroupRole": "string"
+    }
+  },
+  "function": {
+    "AdminQueriesbc4092b1": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "InsertUserCognitoToDataStore": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "geo": {
+    "MyGeoCodeResource": {
+      "Arn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s3funkidsentertainmentstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
