cf4f4c765df1873f7e4cc19491bb670f2d058a4d
alexandrabfreire <alexandrabfreire@gmail.com>
2023-08-21 13:58:47 +0100

    Contact us form update

--- src/components/pages/ContactUs.jsx
+++ src/components/pages/ContactUs.jsx
@@ -1,33 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "@aws-amplify/ui-react/styles.css";
 import ContactUsForm from "../../ui-components/ContactUsForm";
 import { DataStore } from "aws-amplify";
 import { ContactUs as ContactUsModel } from "../../models";
+import { Alert } from "@aws-amplify/ui-react";
 
 function ContactUs() {
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    const firstName = e.target.firstname.value;
-    const email = e.target.email.value;
-    const feedbackType = e.target.feedbackType.value;
-    const overallSiteRating = e.target.overallSiteRating.value;
-    const message = e.target.message.value;
+  const [showForm, setShowForm] = useState(true);
+  const [alert, setAlert] = useState(null);
 
-    await DataStore.save(
-      new ContactUsModel({
-        firstname: firstName,
-        email: email,
-        feedbackType: feedbackType,
-        overallSiteRating: overallSiteRating,
-        message: message,
-      })
-    );
+  const handleFormSubmit = async (fields) => {
+    const trimmedFields = {
+      firstname: fields.firstname.trim(),
+      email: fields.email.trim(),
+      feedbackType: fields.feedbackType,
+      overallSiteRating: fields.overallSiteRating,
+      message: fields.message.trim(),
+    };
+
+    try {
+      await DataStore.save(new ContactUsModel(trimmedFields));
+      setAlert({ type: "success", message: "Thanks for your feedback!" });
+      setShowForm(false);
+    } catch (error) {
+      console.error("Error saving the form data:", error);
+      setAlert({
+        type: "error",
+        message: "There was an error saving your feedback.",
+      });
+    }
   };
 
   return (
     <div className="contact-us">
+      {alert && <Alert variation={alert.type}>{alert.message}</Alert>}
+
       <div className="form-container">
-        <ContactUsForm onSubmit={handleFormSubmit} />
+        {showForm && <ContactUsForm onSubmit={handleFormSubmit} />}
       </div>
     </div>
   );
