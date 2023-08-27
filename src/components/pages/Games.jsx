4959a6fbbc7ee13e4d45befcbf35be1cedf8d525
alexandrabfreire <alexandrabfreire@gmail.com>
2023-08-27 03:09:16 +0100

    Update games page

--- src/components/pages/Games.jsx
+++ src/components/pages/Games.jsx
@@ -1,12 +1,14 @@
 import "../../App.css";
 import { GamesBanner } from "../sections/Banner";
 import CustomDivider from "../sections/Divider";
+import GamesCardCollection from "../../ui-components/GamesCardCollection";
 
 function Games() {
   return (
     <>
       <GamesBanner />
       <CustomDivider />
+      <GamesCardCollection />
     </>
   );
 }
